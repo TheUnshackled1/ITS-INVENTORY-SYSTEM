43e83fce7e296b8dbb7fbe5b8590deb225991e5a
Christian Anthony Gemelo <xtian23anthony@gmail.com>
2026-07-06 10:04:52 +0800

    add inventory management features with views, templates, and URL routing

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Inventory
 
-# Register your models here.
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+	list_display = (
+		'item_type',
+		'brand',
+		'model',
+		'serial_number',
+		'quantity',
+		'location',
+		'status',
+		'date_inventory',
+	)
+	list_filter = ('status', 'location', 'brand', 'date_inventory')
+	search_fields = ('item_type', 'brand', 'model', 'serial_number', 'location')
+	ordering = ('item_type', 'serial_number')
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Inventory
+
+
+def inventory_list(request):
+	inventory_items = Inventory.objects.all().order_by('item_type', 'serial_number')
+	return render(
+		request,
+		'inventory.html',
+		{
+			'inventory_items': inventory_items,
+		},
+	)
