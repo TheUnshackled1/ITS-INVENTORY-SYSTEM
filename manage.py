4dbc4f4c77402271ffb24c217a34ca68f17d1ee9
John Tyrone <jtcoronel.chmsu@gmail.com>
2026-07-21 11:57:51 +0800

    remove redundant docstring from manage.py main function

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
 import os
 import sys
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'its_inventory.settings')
     try:
         from django.core.management import execute_from_command_line
